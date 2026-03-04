3123eaf1c810461f2d059e6540bffbc7324f3ea8
supertx <1374411672@qq.com>
2026-03-04 20:30:27 +0800

    up

--- model/__init__.py
+++
@@ -1,4 +0,0 @@
-from .arcface_head import ArcfaceHead
-from .face_net import get_mbf
-from .palm_net import PalmNet
-from .quality_net import MultiModalQualityNet
--- script/__init__.py
+++
@@ -1,5 +0,0 @@
-from .palm_pretrain import palm_pretrain
-# from .palm_finetune import finetune
-
-from .face_pretrain import face_pretrain    
-from .fusion_train import fusion_trainer
